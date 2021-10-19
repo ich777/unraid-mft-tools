3f7b71c5f23f3af864e67c40e73d6cc89e933c42
ich777 <christophhummer@gmail.com>
2021-10-19 09:11:06 +0200

    „source/compile.sh“ ändern

--- source/compile.sh
+++ source/compile.sh
@@ -1,43 +1,29 @@
 #Download and compile Mellanox Firmware Tools
 cd ${DATA_DIR}
-mkdir ${DATA_DIR}/mlx-v${MLX_MFT_V}
-wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/mlx-v${MLX_MFT_V}.tar.gz https://github.com/Mellanox/mstflint/releases/download/v${MLX_MFT_V}/mstflint-${MLX_MFT_V}.tar.gz
-tar -C ${DATA_DIR}/mlx-v${MLX_MFT_V} --strip-components=1 -xf ${DATA_DIR}/mlx-v${MLX_MFT_V}.tar.gz
-cd ${DATA_DIR}/mlx-v${MLX_MFT_V}
-${DATA_DIR}/mlx-v${MLX_MFT_V}/configure --enable-openssl --prefix=/usr
+mkdir ${DATA_DIR}/mlx-v${LAT_V}
+wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/mlx-v${LAT_V}.tar.gz https://github.com/Mellanox/mstflint/releases/download/v${LAT_V}/mstflint-${LAT_V}.tar.gz
+tar -C ${DATA_DIR}/mlx-v${LAT_V} --strip-components=1 -xf ${DATA_DIR}/mlx-v${LAT_V}.tar.gz
+cd ${DATA_DIR}/mlx-v${LAT_V}
+${DATA_DIR}/mlx-v${LAT_V}/configure --enable-openssl --prefix=/usr
 make -j${CPU_COUNT}
 make DESTDIR=${DATA_DIR}/MFT install -j${CPU_COUNT}
 cd ${DATA_DIR}
-mkdir -p cd ${DATA_DIR}/v${MLX_MFT_V}
+mkdir -p ${DATA_DIR}/v${LAT_V}
 
 #Download and move mlx_temp to destination
 wget -q -nc --show-progress --progress=bar:force:noscroll https://github.com/ich777/runtimes/raw/master/mlx/mlx_temp.zip
 unzip ${DATA_DIR}/mlx_temp.zip -d ${DATA_DIR}/MFT/usr/bin
 
-#Get source package and move files to destination
+#Download and move sourcepackage to destination
 wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/sourcepackage.txz https://github.com/ich777/unraid-mft-tools/raw/master/source/sourcepackage.txz
 tar -C ${DATA_DIR}/MFT/ -xvf ${DATA_DIR}/sourcepackage.txz
+cd ${DATA_DIR}/MFT/
 
 #Create Slackware package
-PLUGIN_NAME="mfttools"
-BASE_DIR="${DATA_DIR}/MFT"
-TMP_DIR="/tmp/${PLUGIN_NAME}_"$(echo $RANDOM)""
-VERSION="$(date +'%Y.%m.%d')"
+makepkg -l n -c n ${DATA_DIR}/v$LAT_V/${APP_NAME}-"$(date +'%Y.%m.%d')".txz
+cd ${DATA_DIR}/v$LAT_V
+md5sum ${APP_NAME}-"$(date +'%Y.%m.%d')".txz | awk '{print $1}' > ${APP_NAME}-"$(date +'%Y.%m.%d')".txz.md5
 
-mkdir -p $TMP_DIR/$VERSION
-cd $TMP_DIR/$VERSION
-cp -R $BASE_DIR/* $TMP_DIR/$VERSION/
-mkdir $TMP_DIR/$VERSION/install
-tee $TMP_DIR/$VERSION/install/slack-desc <<EOF
-       |-----handy-ruler------------------------------------------------------|
-$PLUGIN_NAME: $PLUGIN_NAME Package contents:
-$PLUGIN_NAME:
-$PLUGIN_NAME: Mellanox Firmware Tools v${MLX_MFT_V}
-$PLUGIN_NAME:
-$PLUGIN_NAME:
-$PLUGIN_NAME: Custom MFT package for Unraid by ich777
-$PLUGIN_NAME:
-EOF
-makepkg -l n -c n ${DATA_DIR}/v$MLX_MFT_V/${PLUGIN_NAME}-"$(date +'%Y.%m.%d')".txz
-cd ${DATA_DIR}/v$MLX_MFT_V
-md5sum ${PLUGIN_NAME}-"$(date +'%Y.%m.%d')".txz | awk '{print $1}' > ${PLUGIN_NAME}-"$(date +'%Y.%m.%d')".txz.md5
+## Cleanup
+rm -R ${DATA_DIR}/mlx-v${LAT_V} ${DATA_DIR}/MFT ${DATA_DIR}/sourcepackage.txz ${DATA_DIR}/mlx_temp.zip ${DATA_DIR}/mlx-v${LAT_V}.tar.gz
+chown $UID:$GID ${DATA_DIR}/v$LAT_V/*
