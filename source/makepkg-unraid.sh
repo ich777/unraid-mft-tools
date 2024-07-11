10e02674665a907ab3c2ce801bb5d617e58397f6
Christoph Hummer <christophhummer@gmail.com>
2024-07-11 18:00:05 +0200

    source/makepkg-unraid.sh aktualisiert

--- source/makepkg-unraid.sh
+++ source/makepkg-unraid.sh
@@ -9,7 +9,7 @@ cd $TMP_DIR/$VERSION
 cp --parents -R $BASE_DIR/$PLUGIN_NAME/ $TMP_DIR/$VERSION/
 chmod -R 755 $TMP_DIR/$VERSION/
 rm $TMP_DIR/$VERSION/$BASE_DIR/$PLUGIN_NAME/README.md
-makepkg -l y -c y $TMP_DIR/sourcepackage.txz
-md5sum $TMP_DIR/sourcepackage.txz | awk '{print $1}' > $TMP_DIR/sourcepackage.txz.md5
+makepkg -l y -c y $TMP_DIR/${PLUGIN_NAME}-$VERSION.txz
+md5sum $TMP_DIR/${PLUGIN_NAME}-$VERSION.txz | awk '{print $1}' > $TMP_DIR/${PLUGIN_NAME}-$VERSION.txz.md5
 rm -R $TMP_DIR/$VERSION/
 chmod 755 $TMP_DIR/*
